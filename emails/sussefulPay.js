6e329aba651b2e0bbb863f62f78329bcacbecd72
Vitalik Nesterov <vitalistarkiii@gmail.com>
2020-08-01 18:12:10 +0300

    cors

--- emails/sussefulPay.js
+++ emails/sussefulPay.js
@@ -1,11 +1,48 @@
 const keys = require('../keys')
+    // <p><b>Способ оплаты: </b>${message.payForm}</p>
 
 module.exports = (body, message) => {
+
+    const adress = message.adress === undefined || message.adress === '' ? '' : `<b>Населенный пункт: </b> ${message.adress}</p>`
+    const street = message.street === undefined || message.street === '' ? '' : `<b>Улица: </b> ${message.street}</p>`
+    const home = message.home === undefined || message.home === '' ? '' : `<b>Дом: </b> ${message.home}</p>`
+    const podezd = message.podezd === undefined || message.podezd === '' ? '' : `<b>Подьезд: </b> ${message.podezd}</p>`
+    const kodDveri = message.kodDveri === undefined || message.kodDveri === '' ? '' : `<b>Код двери: </b> ${message.kodDveri}</p>`
+    const etag = message.etag === undefined || message.etag === '' ? '' : `<b>Этаж: </b> ${message.etag}</p>`
+    const comments = message.comments === undefined || message.comments === '' ? '' : `<b>Комментарий: </b> ${message.comments}</p>`
+    const date = message.date === undefined || message.date === '' ? '' : `<b>Дата доставки: </b> ${message.date}</p>`
+    const time = message.time === undefined || message.time === '' ? '' : `<b>Время доставки: </b> ${message.time}</p>`
+    // console.log(message)
+
 return {
     to: keys.EMAIL_TO,
     from: keys.EMAIL_FROM,
     subject: body || "[No subject]",
-    html: message || "[No message]",
+    html: `
+    <div>
+        <p><b>Имя: </b> ${message.name}</p>
+        <p><b>Телефон: </b>${message.phone}</p>
+        <div>
+        <div style="display: flex; flex-direction: column; align-items: center; justify-content: center;
+        "><p style="font-size: 18px;"><b>Товары: </b></p>${message.product}</div>
+        </div>
+        <p><b>Самовывоз или доставка: </b> ${message.delivery}</p>
+        <p>${date}</p>
+        <p>${time}</p>
+        <p>${adress}</p>
+        <p>${street}</p>
+        <p>${home}</p>
+        <p>${podezd}</p>
+        <p>${etag}</p>
+        <p>${kodDveri}</p>
+        <hr>
+        <p><b>Палочек(шт): </b> ${message.chopsticks}</p>
+        <p><b>Сдачас с суммы: </b> ${message.sdacha}</p>
+        <hr>
+        <p><b>Общая цена к оплате: </b> ${message.totalPrice}</p>
+        <p>${comments}</p>
+    </div>
+    ` || "[No message]",
     }
 }
 
--- index.js
+++ index.js
@@ -1,32 +1,51 @@
-const keys = require('./keys')
-const express = require('express')
-const nodemailer = require('nodemailer')
-const sendgrid = require('nodemailer-sendgrid-transport')
-const payEmail = require('./emails/sussefulPay')
-const path = require('path')
-const PORT = process.env.PORT || 3000
-const app = express()
+const keys = require('./keys');
+const express = require('express');
+const nodemailer = require('nodemailer');
+const sendgrid = require('nodemailer-sendgrid-transport');
+const payEmail = require('./emails/sussefulPay');
+const path = require('path');
+const formData = require("express-form-data");
+const os = require("os");
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
 
 // const router = express.Router()
+// app.use(express.urlencoded({ extended: true }))
 
 const mailer = nodemailer.createTransport(sendgrid({
-    auth: {api_key: keys.SENDGRID_API_KEY}
-}))
+    auth: { api_key: keys.SENDGRID_API_KEY }
+}));
 
-app.use(express.urlencoded({extended: true}))
+const options = {
+    uploadDir: os.tmpdir(),
+    autoClean: true
+};
+
+app.use(express.static(__dirname + "/public"));
+app.use(formData.parse(options));
+app.use(formData.format());
+app.use(formData.stream());
+app.use(formData.union());
 
 app.get('/', (req, res) => {
-    res.status(200)
+    res.status(200);
     res.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
+});
 
-app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'contact.html'))
-})
+const optionsCors = {
+    origin: 'https://svisni-sushi.ru/',
+    optionsSuccessStatus: 200,
+    preflightContinue: false,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
 
-app.post("/", async (req, res) => {
-    await mailer.sendMail(payEmail(req.body.subject, req.body.message))
-    res.redirect('/')
-})
+app.post("/", cors(optionsCors), async (req, res) => {
+    // console.log(req.body);
+    await mailer.sendMail(payEmail("Новый заказ", req.body));
+    // res.redirect('/')
+    res.end()
+});
 
-app.listen(PORT)
+app.listen(PORT);
